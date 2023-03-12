3df59d3eb3be748695fdb21f93644e17e621751b
jelloreda <jaimelloredavelasco@gmail.com>
2023-03-12 12:53:34 +0100

    act 12-03-23

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,3 +1,4 @@
+const { verifyToken } = require("../middlewares/verifyToken")
 const User = require("../models/User.model")
 
 const router = require("express").Router()
@@ -20,5 +21,33 @@ router.get('/getOneUser/:id', (req, res, next) => {
         .then(response => res.json(response))
         .catch(err => console.log(err))
 })
+
+router.put("/editUser/:id", verifyToken, (req, res, next) => {
+
+    const { id } = req.params
+    const { username, email, avatar } = req.body
+
+    const updatedUser = {}
+    if (username) updatedUser.username = username
+    if (email) updatedUser.email = email
+    if (avatar) updatedUser.avatar = avatar
+
+    User
+        .findByIdAndUpdate(id, updatedUser, { new: true })
+        .then(response => res.json(response))
+        .catch(err => next(err))
+})
+
+router.delete("/deleteUser/:id", verifyToken, (req, res, next) => {
+
+    const { id } = req.params
+
+    User
+        .findByIdAndDelete(id)
+        .then(response => res.json(response))
+        .catch(err => next(err))
+})
+
+
 
 module.exports = router
